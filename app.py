51fe868a71bdedee2f0c653036d1f7d7a200e940
MohamedWZS <MohamedWael.31.8.2000@gmail.com>
2022-06-18 13:25:18 +0200

    added sum and equal functions

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from operator import eq
 from tkinter import *
 
 window = Tk()
@@ -9,29 +10,41 @@ def display_num(num):
     screenEntry.insert(0, str(current) + str(num))
 
 def clear_num():
-    screenEntry.delete(0, END)                      # clears the screenEntry input field
+    screenEntry.delete(0, END)
+
+def add_nums():
+    firstNum = screenEntry.get()
+    global fNum                                     # create global var that holds the firsNum
+    fNum = int(firstNum)
+    screenEntry.delete(0, END)
+
+def equal():
+    secondtNum = int(screenEntry.get())
+    screenEntry.delete(0, END)
+    result = secondtNum + fNum
+    screenEntry.insert(0, result)
 
 # create number screen
 screenEntry = Entry(window, width = 36)
 screenEntry.grid(row=0, column=0, columnspan=3)
 
 # define numbers
-num7 = Button(window, text="7", width = 9, command=lambda: display_num(7)).grid(row=1, column=0)
-num8 = Button(window, text="8", width = 9, command=lambda: display_num(8)).grid(row=1, column=1)
-num9 = Button(window, text="9", width = 9, command=lambda: display_num(9)).grid(row=1, column=2)
+num7 = Button(window, text="7", width=9, pady=5, command=lambda: display_num(7)).grid(row=1, column=0)
+num8 = Button(window, text="8", width=9, pady=5, command=lambda: display_num(8)).grid(row=1, column=1)
+num9 = Button(window, text="9", width=9, pady=5, command=lambda: display_num(9)).grid(row=1, column=2)
 
-num4 = Button(window, text="4", width = 9, command=lambda: display_num(4)).grid(row=2, column=0)
-num5 = Button(window, text="5", width = 9, command=lambda: display_num(5)).grid(row=2, column=1)
-num6 = Button(window, text="6", width = 9, command=lambda: display_num(6)).grid(row=2, column=2)
+num4 = Button(window, text="4", width=9, pady=5, command=lambda: display_num(4)).grid(row=2, column=0)
+num5 = Button(window, text="5", width=9, pady=5, command=lambda: display_num(5)).grid(row=2, column=1)
+num6 = Button(window, text="6", width=9, pady=5, command=lambda: display_num(6)).grid(row=2, column=2)
 
-num1 = Button(window, text="1", width = 9, command=lambda: display_num(1)).grid(row=3, column=0)
-num2 = Button(window, text="2", width = 9, command=lambda: display_num(2)).grid(row=3, column=1)
-num3 = Button(window, text="3", width = 9, command=lambda: display_num(3)).grid(row=3, column=2)
+num1 = Button(window, text="1", width=9, pady=5, command=lambda: display_num(1)).grid(row=3, column=0)
+num2 = Button(window, text="2", width=9, pady=5, command=lambda: display_num(2)).grid(row=3, column=1)
+num3 = Button(window, text="3", width=9, pady=5, command=lambda: display_num(3)).grid(row=3, column=2)
 
-num0 = Button(window, text="0", width = 9, command=lambda: display_num(0)).grid(row=4, column=0)
-clear = Button(window, text="C", width = 9, command=clear_num).grid(row=4, column=2)
-equal = Button(window, text="=")
+num0 = Button(window, text="0", width=9, pady=5, command=lambda: display_num(0)).grid(row=4, column=0)
+clear = Button(window, text="Clear", width=20, pady=5, command=clear_num).grid(row=4, column=1, columnspan=2)
 
-plus = Button(window, text="+", width = 9).grid(row=4, column=1)
+plus = Button(window, text="+", width = 9, pady=5, command=add_nums).grid(row=5, column=0)
+equality = Button(window, text="=", width=20, pady=5, command=equal).grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
